268606f2e35d0808bd863b20ae0255f9358e1493
Jazzie Juevesano <jazziejuevesano13@gmail.com>
2020-07-27 18:39:57 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Jazzie Juevesano on 7/27/20.
+//  Copyright © 2020 Jazzie Juevesano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
